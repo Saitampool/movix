35f460a73bef13f4cdd1a5b4b242a0413dd11f0f
Saitampool <dimashasim1106@gmail.com>
2024-01-11 18:16:23 +0700

    add component banner

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import useFetch from "../../hooks/useFetch";
+
 import "./style.scss";
 
 function Details() {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+
   return <div>Details</div>;
 }
 
