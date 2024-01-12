2947e291b68f291293b30d7ac1f9a950a38de69b
Saitampool <dimashasim1106@gmail.com>
2024-01-12 17:29:49 +0700

    add poster, rating in detail page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
-import useFetch from "../../hooks/useFetch";
-
 import "./style.scss";
+
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 function Details() {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
-  return <div>Details</div>;
+  return (
+    <div>
+      <DetailsBanner />
+    </div>
+  );
 }
 
 export default Details;
