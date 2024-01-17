466806e62d8d84fd23f9db6457d99f225803d969
Saitampool <dimashasim1106@gmail.com>
2024-01-17 19:37:15 +0700

    add similar and recommendations section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -7,6 +7,8 @@ import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
+import Similar from "./carousels/Similar";
+import Recommendation from "./carousels/Recommendation";
 
 function Details() {
   const { mediaType, id } = useParams();
@@ -20,6 +22,8 @@ function Details() {
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
       <Cast data={credits?.cast} loading={creditsLoading} />
       <VideosSection data={data} loading={loading} />
+      <Similar mediaType={mediaType} id={id} />
+      <Recommendation mediaType={mediaType} id={id} />
     </div>
   );
 }
